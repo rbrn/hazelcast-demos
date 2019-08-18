7447335fecf05ed57565c024005a495d3d8b074d
Costin <costinaldea@yahoo.co.uk>
2019-08-18 22:56:13 +0300

    initial commit

---
+++ client/src/test/java/com/pluralsight/hazelcast/client/services/reporting/DistributedSetTest.java
@@ -0,0 +1,149 @@
+package com.pluralsight.hazelcast.client.services.reporting;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.RingbufferConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IAtomicReference;
+import com.hazelcast.core.IMap;
+import com.hazelcast.core.IQueue;
+import com.hazelcast.ringbuffer.OverflowPolicy;
+import com.hazelcast.ringbuffer.Ringbuffer;
+import com.pluralsight.hazelcast.client.HazelcastClientTestConfiguration;
+import com.pluralsight.hazelcast.client.helper.StorageNodeFactory;
+import com.pluralsight.hazelcast.shared.Transaction;
+import com.pluralsight.hazelcast.storage.StorageNodeApplication;
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Grant Little (grant@grantlittle.me)
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(
+        classes = {
+                HazelcastClientTestConfiguration.class,
+                StorageNodeApplication.class
+        }
+)
+public class DistributedSetTest {
+
+    @Autowired
+    ReportingService reportingService;
+
+    @Autowired
+    @Qualifier("ClientInstance")
+    private HazelcastInstance hazelcastInstance;
+
+    @Test
+    public void testGetIncome() throws Exception {
+
+        Set<Transaction> transactions = hazelcastInstance.getSet("SetDemo");
+
+        transactions.add(createTransation(1L));
+        transactions.add(createTransation(2L));
+
+        transactions.stream().forEach(
+                f -> System.out.println(f.toString())
+        );
+    }
+
+    @Test
+    public void testGetQueue() throws Exception {
+
+        Queue<Transaction> transactions = hazelcastInstance.getQueue("QueueDemo");
+
+        IntStream.range(1, 100).forEach(i -> transactions.offer(createTransation(Long.valueOf(i))));
+
+        try {
+            Transaction firstTransaction = ((IQueue<Transaction>) transactions).poll(1000, TimeUnit.MILLISECONDS);
+            Transaction lastTransaction = transactions.peek();
+            assertTrue(firstTransaction.getCustomerId().equals(1L));
+            assertTrue(lastTransaction.getCustomerId().equals(100L));
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetRingBuffer() throws Exception {
+
+        Ringbuffer<Transaction> transactions = hazelcastInstance.getRingbuffer("RingBufferDemo");
+
+        IntStream.rangeClosed(1, 10).forEach(i -> transactions.addAsync(createTransation(Long.valueOf(i)), OverflowPolicy.OVERWRITE));
+
+        try {
+            Transaction firstTransaction = transactions.readOne(transactions.tailSequence());
+
+            assertTrue(firstTransaction.getCustomerId().equals(10L));
+            transactions.addAsync(createTransation(Long.valueOf(100)), OverflowPolicy.OVERWRITE);
+            firstTransaction = transactions.readOne(transactions.tailSequence());
+            assertTrue(firstTransaction.getCustomerId().equals(100L));
+
+            assertThat(transactions.size(), is(5L));
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void testAtomicReference() throws Exception {
+
+        IAtomicReference<Transaction> transactions = hazelcastInstance.getAtomicReference("atomicReferenceDemo");
+
+        IntStream.rangeClosed(1, 10).forEach(i -> transactions.setAsync(createTransation(Long.valueOf(i))));
+
+        Transaction firstTransaction = transactions.get();
+
+        assertTrue(firstTransaction.getCustomerId().equals(10L));
+
+    }
+
+    private Transaction createTransation(Long value) {
+        Transaction transaction = new Transaction();
+        transaction.setAmount(BigDecimal.TEN);
+        transaction.setCustomerId(value);
+        transaction.setTransactionDateTime(new Date());
+        System.out.println("Created transaction " + transaction.toString());
+        return transaction;
+    }
+
+
+    public Calendar getCurrentDate() {
+        Calendar cal = Calendar.getInstance();
+        DateUtils.truncate(cal, Calendar.DATE);
+        return cal;
+    }
+
+
+    @Bean(name = "ClientInstance")
+    public HazelcastInstance clientInstance(StorageNodeFactory storageNodeFactory, ClientConfig config) throws Exception {
+        //Ensure there is at least 1 running instance();
+        storageNodeFactory.ensureClusterSize(1);
+        return HazelcastClient.newHazelcastClient(config);
+    }
+
+
+}
---
+++ storage-node/src/main/java/com/pluralsight/hazelcast/storage/StorageNodeApplication.java
@@ -0,0 +1,113 @@
+package com.pluralsight.hazelcast.storage;
+
+import com.hazelcast.config.*;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ItemEvent;
+import com.hazelcast.core.ItemListener;
+import com.pluralsight.hazelcast.shared.MapNames;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import static java.util.Arrays.asList;
+
+@Configuration
+@SpringBootApplication
+@ComponentScan
+@EnableJpaRepositories
+public class StorageNodeApplication implements MapNames {
+
+    @Bean(destroyMethod = "shutdown")
+    public HazelcastInstance createStorageNode(
+            @Qualifier("StorageNodeConfig")
+            Config config
+    ) throws Exception {
+        return Hazelcast.newHazelcastInstance(config);
+    }
+
+
+
+    @Bean(name="StorageNodeConfig")
+    public Config config(CustomersMapStore customersMapStore) throws Exception {
+        Config config = new Config();
+
+        //Create a new map configuration for the customers map
+        MapConfig customerMapConfig = new MapConfig();
+
+        //Create a map store config for the customer information
+        MapStoreConfig customerMapStoreConfig = new MapStoreConfig();
+        customerMapStoreConfig.setImplementation(customersMapStore);
+        customerMapStoreConfig.setWriteDelaySeconds(3);
+
+        //Update the customers map configuration to use the
+        //customers map store config we just created
+        customerMapConfig.setMapStoreConfig(customerMapStoreConfig);
+        customerMapConfig.setName("customers");
+
+        MapIndexConfig dobFieldIndex = new MapIndexConfig("dob", true);
+        customerMapConfig.addMapIndexConfig(dobFieldIndex);
+
+        //Add the customers map config to our storage node config
+        config.addMapConfig(customerMapConfig);
+
+        config.addSetConfig(createSetConfig());
+
+        config.addQueueConfig(createQueueConfig());
+
+        config.addRingBufferConfig(getRingBufferConfig());
+
+        config.addAtomicReferenceConfig(createAtomicReferenceCOnfig());
+
+
+        return config;
+    }
+
+    private AtomicReferenceConfig createAtomicReferenceCOnfig() {
+        AtomicReferenceConfig atomicReferenceConfig = new AtomicReferenceConfig("atomicReferenceDemo");
+        return atomicReferenceConfig;
+    }
+
+    private QueueConfig createQueueConfig() {
+        QueueConfig queueConfig = new QueueConfig();
+
+        queueConfig.setMaxSize(5);
+        queueConfig.setName("QueueDemo");
+        queueConfig.setStatisticsEnabled(true);
+
+
+        queueConfig.setItemListenerConfigs(asList(new ItemListenerConfig(new ItemListener() {
+            @Override
+            public void itemAdded(ItemEvent itemEvent) {
+                System.out.println(itemEvent.getItem());
+            }
+
+            @Override
+            public void itemRemoved(ItemEvent itemEvent) {
+
+            }
+        }, true)));
+        return queueConfig;
+    }
+
+    private SetConfig createSetConfig() {
+        SetConfig setConfig =new SetConfig();
+        setConfig.setName("SetDemo");
+        return setConfig;
+    }
+
+    private RingbufferConfig getRingBufferConfig(){
+        RingbufferConfig rbConfig = new RingbufferConfig("RingBufferDemo").setCapacity(5);
+        return rbConfig;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageNodeApplication.class, args);
+    }
+
+
+}
